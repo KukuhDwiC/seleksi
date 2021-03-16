cb181b20b69f4f7b509e4eaabd9bebe2a33dc6fd
KukuhDwiC <kukuh1800016088@webmail.uad.ac.id>
2021-03-16 22:08:34 +0800

    projecttest

---
+++ src/app/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  apiUrl:any='https://demo.jejakpustaka.com/api/example/users';
+  constructor(private http: HttpClient) { }
+  baca()
+  {
+    return this.http.get(this.apiUrl);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import {ApiService} from './api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projecttest';
+  constructor(public api:ApiService,)
+  {
+    this.getData();
+  }
+dataUsers:any = [];
+getData()
+{
+    this.api.baca().subscribe(res=>{
+      console.log(res);
+      this.dataUsers = res;
+    })
+}
+
+hapus(n;any){
+  let id = this.dataUsers.indexOf(n);
+  console.log(id)
+  if (id !== null) {
+    this.dataUsers.splice(n,1)
+  }
+}
 }
